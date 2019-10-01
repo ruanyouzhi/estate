6ba5bf147c28f44ba486838e48694ca887b0f159
ruanyouzhi <1830128280@qq.com>
2019-10-01 10:59:07 +0800

    init poj

---
+++ src/main/java/com/ruanyouzhi/estate/estate/EstateApplication.java
@@ -0,0 +1,13 @@
+package com.ruanyouzhi.estate.estate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EstateApplication.class, args);
+    }
+
+}
