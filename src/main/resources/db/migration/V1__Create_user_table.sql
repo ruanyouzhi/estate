f3ba4c6678267b46ad73009b142d38c595b9f77c
ruanyouzhi <1830128280@qq.com>
2019-12-11 00:49:40 +0800

    add database 脚本

--- src/main/resources/db/migration/V1__Create_user_table.sql
+++ src/main/resources/db/migration/V1__Create_user_table.sql
@@ -1,13 +1,9 @@
-create table USER
+create table user
 (
-    ID INTEGER auto_increment,
-    ACCOUNT_ID VARCHAR(100),
-    NAME VARCHAR(50),
-    TOKEN CHAR(36),
-    GMT_CREATE BIGINT,
-    GMT_MODIFIED BIGINT,
-    constraint USER_PK
-        primary key (ID)
+    id int auto_increment primary key not null,
+    account_id varchar(100),
+    name varchar(50),
+    token varchar(36),
+    gmt_create bigint,
+    gmt_modified bigint
 );
-
-
--- src/main/resources/db/migration/V5__Create_comment_table.sql
+++ src/main/resources/db/migration/V5__Create_comment_table.sql
@@ -1,18 +1,10 @@
 create table comment
 (
-    id BIGINT,
-    parent_id BIGINT,
-    type int,
-    commentator BIGINT,
-    gmt_create BIGINT,
-    gmt_modified BIGINT,
-    like_count BIGINT default 0,
-    content VARCHAR(1024),
-    constraint comment_pk
-        primary key (id)
+    id bigint auto_increment primary key,
+    parent_id bigint not null,
+    type int not null,
+    commentator int not null,
+    gmt_create bigint not null,
+    gmt_modified bigint not null,
+    like_count bigint default 0
 );
-
-
-
-
-
