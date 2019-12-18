13bca09e4bc28d4b9a8b5bd1e7d2f38cb14a4bd6
ruanyouzhi <1830128280@qq.com>
2019-12-18 21:56:08 +0800

    add delete question function

--- src/main/java/com/ruanyouzhi/estate/estate/controller/QuestionController.java
+++ src/main/java/com/ruanyouzhi/estate/estate/controller/QuestionController.java
@@ -1,17 +1,17 @@
 package com.ruanyouzhi.estate.estate.controller;
-
+import com.ruanyouzhi.estate.estate.Mapper.QuestionMapper;
 import com.ruanyouzhi.estate.estate.Model.User;
 import com.ruanyouzhi.estate.estate.dto.CommentDTO;
+import com.ruanyouzhi.estate.estate.dto.ResultDTO;
 import com.ruanyouzhi.estate.estate.dto.questionDTO;
 import com.ruanyouzhi.estate.estate.enums.CommentTypeEnum;
+import com.ruanyouzhi.estate.estate.exception.CustomizeErrorCode;
 import com.ruanyouzhi.estate.estate.service.CommentService;
 import com.ruanyouzhi.estate.estate.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-
+import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
@@ -21,6 +21,8 @@ public class QuestionController {
     private QuestionService questionService;
     @Autowired
     private CommentService commentService;
+    @Autowired
+    private QuestionMapper questionMapper;
     @GetMapping("/question/{id}")
     public String question(@PathVariable(name="id") long id,
                            HttpServletRequest request,
@@ -38,5 +40,17 @@ public class QuestionController {
         model.addAttribute("comments",commentDTOList);
         model.addAttribute("relatedQuestions",relatedQuestions);
         return "question";
+    }
+
+    //删除问题
+    @GetMapping("/deleteQuestion/{id}")
+    public Object post(@PathVariable(name="id") long id,
+                       HttpServletRequest request){
+        User user = (User) request.getSession().getAttribute("user");
+        if (user==null){
+            return ResultDTO.errorOf(CustomizeErrorCode.NO_LOGIN);
+        }
+        questionMapper.deleteByPrimaryKey(id);
+        return "redirect:/profile/questions";
     }
 }
--- src/main/resources/static/js/estate.js
+++ src/main/resources/static/js/estate.js
@@ -164,3 +164,35 @@ function likeComment(e) {
     });
 
 }
+function questionDelete(e,status) {
+    if(status==1){
+        var id=e.getAttribute("data-questionId");
+        $.ajax({
+            type: "POST",
+            url: "/deleteQuestion",
+            contentType: "application/json",
+            data: JSON.stringify({
+                "id": id,
+            }),
+            success: function (response) {
+                if (response.code == 200) {
+                    window.location.reload();
+                } else {
+                    if (response.code == 2003) {
+                        var conf = confirm(response.message);
+                        if (conf) {
+                            window.open("https://github.com/login/oauth/authorize?client_id=10c78b0a763c6a093d41&redirect_uri=http://localhost:8080/callback&scope=user&state=1");
+                            window.localStorage.setItem("closable", true);
+                        } else {
+                            alert(response.message);
+                        }
+                    }else{
+                        alert(response.message);
+                    }
+                }
+            },
+            dataType: "json"
+        });
+
+    }
+}
